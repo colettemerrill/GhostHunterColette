e24060d3d9b25a38fb48b9f6ac500292be8f6ff0
ljdescour <ljdescour@gmail.com>
2015-04-08 21:40:01 -0400

    added onPause() methods to Easy,Medium,Hard
    onPause() methods "release" music when paused

--- app/src/main/java/cdm8pf/cs2110/virginia/edu/ghosthunter/Medium.java
+++ app/src/main/java/cdm8pf/cs2110/virginia/edu/ghosthunter/Medium.java
@@ -22,4 +22,12 @@ public class Medium extends Activity {
 
 
     }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        //"releases" or stops music on onPause() method
+        backgroundMusic.release();
+    }
 }
